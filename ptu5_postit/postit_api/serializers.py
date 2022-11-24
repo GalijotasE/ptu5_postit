23b37ed8357652cfa7968951ca47dd93603333fe
GalijotasE <galijotase@gmail.com>
2022-11-24 14:18:09 +0200

    click here to see changes

--- ptu5_postit/postit_api/serializers.py
+++ ptu5_postit/postit_api/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from . import models
 
 class Serializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'body', 'user', 'created_at')
+        fields = ('id', 'title', 'body', 'user', 'user_id', 'created_at')
